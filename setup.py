ae32504695277ddc3e660801d98e742350e91e1f
shortspeed <ssathianathen@gmail.com>
2017-05-19 20:10:59 +1000

    .5

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'fakeUserData',
   packages = ['fakeUserData'], # this must be the same as the name above
-  version = '1.0.4',
+  version = '1.0.5',
   description = 'Generate fake user data',
   author = 'Stefan R. Sathianathen',
   author_email = 'stefan4419@gmail.com',  
   url = 'https://github.com/stefanrsathianathen/fakeUserData', # use the URL to the github repo
-  download_url = 'https://github.com/stefanrsathianathen/fakeUserData/archive/1.0.4.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/stefanrsathianathen/fakeUserData/archive/1.0.5.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'userData', 'data'], # arbitrary keywords
   classifiers = [],
 )
