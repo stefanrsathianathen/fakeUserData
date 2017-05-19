e62d102affeacf91d4279a68a79a90f24136bd32
shortspeed <ssathianathen@gmail.com>
2017-05-19 19:36:20 +1000

    setup1

--- fakeUserData/loadData.py
+++ fakeUserData/loadData.py
@@ -3,50 +3,46 @@
 open and save data into lists (Pretty large datasets)
 '''
 
-import os
-this_dir, this_filename = os.path.split(__file__)
-
-
-text_file = open(os.path.join(this_dir, "data", "domains.txt"),"r")
+text_file = open( "domains.txt", "r")
 domains = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "lastNames.txt"),"r")
+text_file = open("lastNames.txt", "r")
 lastNames = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "firstNames.txt"),"r")
+text_file = open("firstNames.txt", "r")
 firstNames = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "maleNames.txt"),"r")
+text_file = open( "maleNames.txt", "r")
 maleNames = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "femaleNames.txt"),"r")
+text_file = open("femaleNames.txt", "r")
 femaleNames = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "randomChar.txt"),"r")
+text_file = open("randomChar.txt","r")
 randomChar = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "countries.txt"),"r")
+text_file = open("countries.txt","r")
 countries = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "streetNames.txt"),"r")
+text_file = open("streetNames.txt","r")
 streetNames = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "streetSuffixes.txt"),"r")
+text_file = open("streetSuffixes.txt","r")
 streetSuffixes = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "cities.txt"),"r")
+text_file = open("cities.txt","r")
 cities = text_file.read().split(',')
 text_file.close()
 
-text_file = open(os.path.join(this_dir, "data", "states.txt"),"r")
+text_file = open("states.txt","r")
 states = text_file.read().split(',')
 text_file.close()
